fc9bdc9f7f077db1df3e067cefeeb27a18da85ab
Shaun Gordon <shaundev.gordon@gmail.com>
2020-09-20 13:02:47 +0100

    devise installed and configured with basic user model validation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    id {1}
+    first_name {"Test"}
+    last_name {"User"}
+    email {"#{first_name}.#{last_name}@example.com".downcase }
+    password {"qwerty"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'fails to save when first name is not present' do
+    @user = FactoryBot.build(:user, first_name: nil)
+
+    expect(@user).not_to be_valid
+  end
+
+  it 'fails to save when last name is not present' do
+    @user = FactoryBot.build(:user, last_name: nil)
+
+    expect(@user).not_to be_valid
+  end
+
+  it 'fails to save when email is not present' do
+    @user = FactoryBot.build(:user, email: nil)
+    
+    expect(@user).not_to be_valid
+  end
+  
+  it 'fails to save when password is not present' do
+    
+    @user = FactoryBot.build(:user, password: nil)
+    
+    expect(@user).not_to be_valid
+  end
 end
