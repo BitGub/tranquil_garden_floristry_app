e9700272158bf1bfc495bf4694b21f9068e18e5e
Shaun Gordon <shaundev.gordon@gmail.com>
2020-09-22 18:56:31 +0100

    deleted uneeded config files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,6 @@ require 'rspec/rails'
 require 'capybara'
 require 'support/database_cleaner'
 require 'support/factory_bot'
-require 'support/controller_macros'
 require 'support/sessions_helper'
 require 'devise'
 
@@ -19,7 +18,6 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include FactoryBot::Syntax::Methods
-  config.extend ControllerMacros, :type => :controller
 end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
