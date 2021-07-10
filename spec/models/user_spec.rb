27085b58717225570ddfde2098b4e94fa3e8f7ff
Shaun Gordon <shaundev.gordon@gmail.com>
2021-07-10 16:49:41 +0100

    products new and edit forms moved to partials removed User name validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  it 'fails to save when first name is not present' do
-    @user = FactoryBot.build(:user, first_name: nil)
-
-    expect(@user).not_to be_valid
-  end
-
-  it 'fails to save when last name is not present' do
-    @user = FactoryBot.build(:user, last_name: nil)
-
-    expect(@user).not_to be_valid
-  end
-
   it 'fails to save when email is not present' do
     @user = FactoryBot.build(:user, email: nil)
     
